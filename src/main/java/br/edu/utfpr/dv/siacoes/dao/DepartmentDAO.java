20f312f2ca28d96f6fd47726b9f33b00ecc9acea
Patricia Santos <patricia_saantos@live.com>
2020-08-18 21:46:53 -0300

    Uni-se as funções de listas todos os departamentos juntamento com listar por campus, através de uma condição para ajustar a classe as condições dos principios S.O.L.I.D, aumentando a abstração e o princípio de responsabilidade única

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/DepartmentDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/DepartmentDAO.java
@@ -45,6 +45,7 @@ public class DepartmentDAO {
 		}
 	}
 	
+	/*
 	public List<Department> listAll(boolean onlyActive) throws SQLException{
 		Connection conn = null;
 		Statement stmt = null;
@@ -73,20 +74,32 @@ public class DepartmentDAO {
 			if((conn != null) && !conn.isClosed())
 				conn.close();
 		}
-	}
+	}*/ 
+	
 	
 	public List<Department> listByCampus(int idCampus, boolean onlyActive) throws SQLException{
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
 		
-			rs = stmt.executeQuery("SELECT department.*, campus.name AS campusName " +
-					"FROM department INNER JOIN campus ON campus.idCampus=department.idCampus " +
-					"WHERE department.idCampus=" + String.valueOf(idCampus) + (onlyActive ? " AND department.active=1" : "") + " ORDER BY department.name");
+		//----------Lista all na função listar por campus
+		try {
+            conn = ConnectionDAO.getInstance().getConnection();
+            stmt = conn.createStatement();
+        if (idCampus != null && idCampus > 0) {
+            rs = stmt.executeQuery("SELECT department.*, campus.name AS campusName " +
+                "FROM department INNER JOIN campus ON campus.idCampus=department.idCampus " +
+                "WHERE department.idCampus=" + String.valueOf(idCampus) + (onlyActive ? " AND department.active=1" : "") + " ORDER BY department.name");
+        } else {
+
+            rs = stmt.executeQuery("SELECT department.*, campus.name AS campusName " +
+                "FROM department INNER JOIN campus ON campus.idCampus=department.idCampus " +
+                (onlyActive ? " WHERE department.active=1" : "") + " ORDER BY department.name");
+        }
+		
+			
+		}
 			
 			List<Department> list = new ArrayList<Department>();
 			
@@ -104,6 +117,7 @@ public class DepartmentDAO {
 				conn.close();
 		}
 	}
+	//----------------------------------------------------------------------------
 	
 	public int save(int idUser, Department department) throws SQLException{
 		boolean insert = (department.getIdDepartment() == 0);
